0f78082c74e18699db52d061a8a116ae2773063d
Laura Romero <lromero@ccs.neu.edu>
2019-07-27 20:52:01 -0400

    rev

--- week4/partition-equal-subset-sum.py
+++ week4/partition-equal-subset-sum.py
@@ -1,28 +1,24 @@
 #[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,100]
 
 class Solution:
-    num_buckets = 2
-    target = 0
-
     def can_partition(self, a_sum: int, b_sum: int, nums: List[int]) -> bool:
         print(f"a_sum:{a_sum},b_sum:{b_sum}")
-        if a_sum == self.target == b_sum:
+        if a_sum == b_sum:
             return True
         elif b_sum > 0:
             for i in range(len(nums)):
                 num = nums[i]
                 if a_sum + num <= b_sum - num and self.can_partition(a_sum + num, b_sum - num, nums[i+1:]):
                     return True
-        else:
-            return False
+
+        return False
 
 
 
     def canPartition(self, nums: List[int]) -> bool:
         print("==========================================")
         total = sum(nums)
-        if total % self.num_buckets == 0:
-            self.target = total / self.num_buckets
+        if total % 2 == 0:
             return self.can_partition(0, total, nums)
-        else:
-            return False
+
+        return False
